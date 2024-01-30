570260321b6a9472e7dd6874d479a3252a663d3a
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-01-30 12:58:25 +0100

    Add files via upload

---
+++ zadatak1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 { 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Unesite rečenicu:");
+        string recenica = Console.ReadLine();
+
+        string zamijenjenaRecenica = recenica.Replace(" ", "_");
+
+        Console.WriteLine(zamijenjenaRecenica);
+        Console.ReadKey();
+    }
+}
+}
